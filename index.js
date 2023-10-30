02c40803499b8cf947e7416b92894a43071a4598
Daniel Lucas <149091859+WebDesignGTM@users.noreply.github.com>
2023-10-29 23:48:43 -0600

    Proyecto finalizado
    Web Scrapping colaborativo

--- index.js
+++ index.js
@@ -1,3 +1,234 @@
+<<<<<<< HEAD:webscraping/index.js
+const express=require("express");
+const app=express();
+var mysql=require("mysql");
+const bodyPareser=require("body-parser");
+const { json } = require("express");
+app.use(bodyPareser.urlencoded({extended:false}));
+app.use(bodyPareser.json());
+const config ={
+    host: 'b9bgqyullwkheivnwahf-mysql.services.clever-cloud.com',
+    user:'uadufcthwtqmsupj',
+    password:'f4lDaDIL9tXlI666QPLC',
+    database:'b9bgqyullwkheivnwahf',
+    port:'3306'
+
+};
+
+/* laptop */
+const pool=mysql.createPool(config);
+app.get('/laptops',(request,response)=>{
+    response.sendFile('laptops.html',{root:__dirname});
+
+    console.log("se antendio una solicitud");
+});
+
+app.get('/ver/', (request,response)=>{
+		
+    pool.query("SELECT * FROM laptops; ",(errror,result)=>{
+        
+        if(errror) throw errror;
+        response.json(result);
+        console.log(result);
+    });
+})
+/* mouse */
+
+app.get('/mouse',(request,response)=>{
+    response.sendFile('mouse.html',{root:__dirname});
+
+    console.log("se antendio una solicitud");
+});
+
+app.get('/ver2/', (request,response)=>{
+		
+    pool.query("SELECT * FROM mouse; ",(errror,result)=>{
+        
+        if(errror) throw errror;
+        response.json(result);
+        console.log(result);
+    });
+})
+/*teclado */
+app.get('/teclado',(request,response)=>{
+    response.sendFile('teclado.html',{root:__dirname});
+
+    console.log("se antendio una solicitud");
+});
+
+app.get('/ver3/', (request,response)=>{
+		
+    pool.query("SELECT * FROM teclados; ",(errror,result)=>{
+        
+        if(errror) throw errror;
+        response.json(result);
+        console.log(result);
+    });
+})
+/*monitor */
+app.get('/monitor',(request,response)=>{
+    response.sendFile('monitor.html',{root:__dirname});
+
+    console.log("se antendio una solicitud");
+});
+
+app.get('/ver4/', (request,response)=>{
+		
+    pool.query("SELECT * FROM monitores; ",(errror,result)=>{
+        
+        if(errror) throw errror;
+        response.json(result);
+        console.log(result);
+    });
+})
+/*audifonos */
+app.get('/audifonos',(request,response)=>{
+    response.sendFile('audifonos.html',{root:__dirname});
+
+    console.log("se antendio una solicitud");
+});
+
+app.get('/ver5/', (request,response)=>{
+		
+    pool.query("SELECT * FROM audifonos; ",(errror,result)=>{
+        
+        if(errror) throw errror;
+        response.json(result);
+        console.log(result);
+    });
+})
+
+/*motherboard */
+app.get('/motherboard',(request,response)=>{
+    response.sendFile('placaM.html',{root:__dirname});
+
+    console.log("se antendio una solicitud");
+});
+
+app.get('/ver6/', (request,response)=>{
+		
+    pool.query("SELECT * FROM motherboard; ",(errror,result)=>{
+        
+        if(errror) throw errror;
+        response.json(result);
+        console.log(result);
+    });
+})
+/*procesadores */
+app.get('/procesador',(request,response)=>{
+    response.sendFile('procesadores.html',{root:__dirname});
+
+    console.log("se antendio una solicitud");
+});
+
+app.get('/ver7/', (request,response)=>{
+		
+    pool.query("SELECT * FROM procesadores; ",(errror,result)=>{
+        
+        if(errror) throw errror;
+        response.json(result);
+        console.log(result);
+    });
+})
+/*tarjetaG */
+app.get('/TarjetaMadre',(request,response)=>{
+    response.sendFile('trajetaG.html',{root:__dirname});
+
+    console.log("se antendio una solicitud");
+});
+
+app.get('/ver8/', (request,response)=>{
+		
+    pool.query("SELECT * FROM tarjeta_video; ",(errror,result)=>{
+        
+        if(errror) throw errror;
+        response.json(result);
+        console.log(result);
+    });
+})
+/*SSD */
+app.get('/ssd',(request,response)=>{
+    response.sendFile('ssd.html',{root:__dirname});
+
+    console.log("se antendio una solicitud");
+});
+
+app.get('/ver9/', (request,response)=>{
+		
+    pool.query("SELECT * FROM ssd; ",(errror,result)=>{
+        
+        if(errror) throw errror;
+        response.json(result);
+        console.log(result);
+    });
+})
+/*ram */
+app.get('/ram',(request,response)=>{
+    response.sendFile('ram.html',{root:__dirname});
+
+    console.log("se antendio una solicitud");
+});
+
+app.get('/ver10/', (request,response)=>{
+		
+    pool.query("SELECT * FROM ram; ",(errror,result)=>{
+        
+        if(errror) throw errror;
+        response.json(result);
+        console.log(result);
+    });
+})
+/*fuente de poder*/
+app.get('/FuenteP',(request,response)=>{
+    response.sendFile('fuenteP.html',{root:__dirname});
+
+    console.log("se antendio una solicitud");
+});
+
+app.get('/ver11/', (request,response)=>{
+		
+    pool.query("SELECT * FROM fuente_poder; ",(errror,result)=>{
+        
+        if(errror) throw errror;
+        response.json(result);
+        console.log(result);
+    });
+})
+/*ventiladores*/
+app.get('/ventiladores',(request,response)=>{
+    response.sendFile('ventiladores.html',{root:__dirname});
+
+    console.log("se antendio una solicitud");
+});
+
+app.get('/ver12/', (request,response)=>{
+		
+    pool.query("SELECT * FROM ventiladores; ",(errror,result)=>{
+        
+        if(errror) throw errror;
+        response.json(result);
+        console.log(result);
+    });
+})
+/*cases*/
+app.get('/cases',(request,response)=>{
+    response.sendFile('cases.html',{root:__dirname});
+
+    console.log("se antendio una solicitud");
+});
+
+app.get('/ver13/', (request,response)=>{
+		
+    pool.query("SELECT * FROM cases; ",(errror,result)=>{
+        
+        if(errror) throw errror;
+        response.json(result);
+        console.log(result);
+    });
+})
+app.listen(5555,()=>{
+    console.log("servicio activo");
+=======
 const express=require("express");
 const app=express();
 var mysql=require("mysql");
@@ -34,4 +265,5 @@ app.get('/ver/', (request,response)=>{
 
 app.listen(5555,()=>{
     console.log("servicio activo");
+>>>>>>> 646dc4e4c8f9cf856792da1441fdd898041589a4:index.js
 });
